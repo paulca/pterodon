bdcc39a6679149e29dd9d1ec8438da13d4c44a71
Paul Campbell <paul@tito.io>
2026-02-25 12:51:44 +0000

    Fix actor URL parsing

--- app/controllers/activity_pub/shared_inbox_controller.rb
+++ app/controllers/activity_pub/shared_inbox_controller.rb
@@ -7,26 +7,43 @@ module ActivityPub
       request.body.rewind
       activity = ActivityPub::Activity.new(body)
 
-      case activity.type
-      when 'Follow'
-        target_username = activity.object.split('/').last
-        user = User.find_by!(username: target_username)
+      user = resolve_target_user(activity)
+      if user
         ProcessActivityJob.perform_later(activity.to_h, user.id)
-      when 'Undo'
-        inner = activity.object
-        if inner.is_a?(Hash) && inner['type'] == 'Follow'
-          target_username = inner['object'].to_s.split('/').last
-          user = User.find_by!(username: target_username)
-          ProcessActivityJob.perform_later(activity.to_h, user.id)
-        end
+      else
+        Rails.logger.info "SharedInbox: Could not resolve target user for #{activity.type} from #{activity.actor}"
       end
 
       head :accepted
-    rescue JSON::ParserError, KeyError
+    rescue JSON::ParserError => e
+      Rails.logger.warn "SharedInbox: Invalid JSON: #{e.message}"
       head :bad_request
     end
 
     private
+
+    def resolve_target_user(activity)
+      case activity.type
+      when 'Follow'
+        extract_user_from_actor_uri(activity.object)
+      when 'Undo'
+        inner = activity.object
+        if inner.is_a?(Hash) && inner['type'] == 'Follow'
+          extract_user_from_actor_uri(inner['object'])
+        end
+      end
+    end
+
+    def extract_user_from_actor_uri(object)
+      uri = case object
+            when String then object
+            when Hash then object['id']
+            end
+      return unless uri.is_a?(String)
+
+      match = URI.parse(uri).path.match(%r{/activity_pub/([^/]+)/actor})
+      User.find_by(username: match[1]) if match
+    end
 
     def verify_http_signature!
       HttpSignatureVerifier.new(request).verify!
