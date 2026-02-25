8c2b0734305521cac3616ff5a1afce9d39d7e48b
Paul Campbell <paul@tito.io>
2026-02-25 21:45:32 +0000

    That'll do for now (#49)

--- app/models/remote_reply.rb
+++ app/models/remote_reply.rb
@@ -1,4 +1,7 @@
 class RemoteReply < ApplicationRecord
+  ALLOWED_TAGS = %w[p br a strong em b i blockquote ul ol li code pre span].freeze
+  ALLOWED_ATTRIBUTES = %w[href rel class].freeze
+
   belongs_to :post
 
   validates :activity_uri, presence: true, uniqueness: true
