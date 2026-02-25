9c4c373141e76a0154d60fde4a7e40a56146f8d9
Paul Campbell <paul@tito.io>
2026-02-25 13:01:25 +0000

    Webfinger validations

--- app/controllers/activity_pub/webfinger_controller.rb
+++ app/controllers/activity_pub/webfinger_controller.rb
@@ -1,11 +1,21 @@
 module ActivityPub
-  class WebfingerController < ActionController::Base
+  class WebfingerController < ApplicationController
+    allow_unauthenticated_access
+    skip_before_action :verify_authenticity_token
+
     def show
       resource = params[:resource]
-      return head :not_found if resource.blank?
+      return head :bad_request if resource.blank?
 
-      username = resource.split('@').first.sub('acct:', '')
-      @user = User.find_by!(username: username)
+      match = resource.match(/\Aacct:([^@]+)@(.+)\z/)
+      return head :bad_request unless match
+
+      username = match[1]
+      domain = match[2]
+      return head :not_found unless domain == request.host
+
+      @user = User.find_by(username: username)
+      return head :not_found unless @user
 
       render json: {
         subject: "acct:#{@user.username}@#{request.host}",
