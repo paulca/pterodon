4b3037c35508ee2c90da33fe0dfdae35af42ac1d
Paul Campbell <paul@tito.io>
2026-03-02 14:18:41 +0000

    Back off retries

--- app/jobs/bluesky/deliver_delete_job.rb
+++ app/jobs/bluesky/deliver_delete_job.rb
@@ -1,6 +1,7 @@
 module Bluesky
   class DeliverDeleteJob < ApplicationJob
     queue_as :default
+    retry_on PostDeliveryService::DeliveryError, wait: :polynomially_longer, attempts: 10
 
     # We accept bsky_uri + user_id since the post is already destroyed
     # by the time this job runs.
@@ -9,6 +10,9 @@ module Bluesky
       return unless user&.bluesky_configured?
 
       PostDeliveryService.new(user).delete(bsky_uri)
+    rescue PostDeliveryService::DeliveryError => e
+      Rails.logger.error "Bluesky delete failed for uri=#{bsky_uri} user=#{user_id}: #{e.message}"
+      raise
     end
   end
 end
--- app/jobs/bluesky/deliver_post_job.rb
+++ app/jobs/bluesky/deliver_post_job.rb
@@ -1,6 +1,7 @@
 module Bluesky
   class DeliverPostJob < ApplicationJob
     queue_as :default
+    retry_on PostDeliveryService::DeliveryError, wait: :polynomially_longer, attempts: 5
 
     def perform(post_id)
       post = Post.find_by(id: post_id)
@@ -9,6 +10,9 @@ module Bluesky
       return unless post.user.bluesky_configured?
 
       PostDeliveryService.new(post.user).deliver(post)
+    rescue PostDeliveryService::DeliveryError => e
+      Rails.logger.error "Bluesky delivery failed for post #{post_id}: #{e.message}"
+      raise
     end
   end
 end
