3e485a9f1e6d034578b0c6dd8bb1e8f897a30bc5
Paul Campbell <paul@tito.io>
2026-03-02 14:22:03 +0000

    Rescue more

--- app/jobs/bluesky/sync_replies_job.rb
+++ app/jobs/bluesky/sync_replies_job.rb
@@ -7,8 +7,10 @@ module Bluesky
         next unless user.bluesky_configured?
 
         ReplySyncService.new(user).sync_all
-      rescue ReplySyncService::SyncError => e
+      rescue ReplySyncService::SyncError, Client::Error => e
         Rails.logger.error "Bluesky reply sync failed for user #{user.id}: #{e.message}"
+      rescue StandardError => e
+        Rails.logger.error "Unexpected error during Bluesky reply sync for user #{user.id}: #{e.class} - #{e.message}"
       end
     end
   end
